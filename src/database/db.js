d3617a26f467f930108f443172f06b6bfd20bc43
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-11-23 19:13:06 -0300

    refact: database

--- src/database/db.js
+++ src/database/db.js
@@ -1,3 +1,16 @@
+import dotenv from "dotenv";
+import { MongoClient } from 'mongodb';
+dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URI)
-const db = mongoClient('8styles')
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+try {
+    await mongoClient.connect();
+    console.log("MongoDB conectado");
+} catch (err) {
+    console.log(err);
+}
+
+const db = mongoClient.db("8Styles");
+export const usersCollection = db.collection("users");
+export const sessionsCollection = db.collection("sessions");
+export const productsCollection = db.collection("products");
