5072d0fa3787994be84743b824d1867142cf1585
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-11-23 20:08:20 -0300

    feat: sing-up middleware

---
+++ src/Middlewares/sign-unValidate.middleware.js
@@ -0,0 +1,31 @@
+import signupSchema from "../Models/sign-up.model";
+import { usersCollection} from "../database/db";
+
+export async function signupValidation(req, res, next)  {
+    const user = req.body;
+
+    if (req.body.password !== req.body.confirmedPass) {
+        return res.status(409).send({ message: "As duas senhas devem ser iguais" })
+    }
+
+    try {
+        const userExists = await usersCollection.findOne({ email });
+
+        if (userExists) {
+            return res.status(409).send({ message: "Esse email já existe" });
+        }
+
+        const { error } = signupSchema.validate(user, { abortEarly: false});
+
+        if (error) {
+            const errors = error.details.map((detail) => detail.message);
+            return res.status(422).send( {message: errors });
+        }
+
+        delete user.confirmedPass;
+        req.user = user;
+    } catch {
+        return res.sendStatus(500);
+    }
+
+}
--- src/Models/sign-up.model.js
+++ src/Models/sign-up.model.js
@@ -1,10 +1,10 @@
 import joi from "joi";
 
-const singupSchema = joi.object({
+const signupSchema = joi.object({
     name: joi.string().alphanum().max(10).min(3).required(),
     email: joi.string().email().required(),
     password: joi.string().min(6).required(),
     confirmedPass: joi.string().required()
   });
   
-  export default singupSchema;
+  export default signupSchema;
