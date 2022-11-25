a936767596d718089aa497f9361bd1457a8152fa
Igor de Castro <igordecastro07@gmail.com>
2022-11-24 21:19:35 -0300

    feat: get cart list

--- src/Controllers/cartController.js
+++ src/Controllers/cartController.js
@@ -2,7 +2,7 @@ import { cartCollection } from "../database/db.js";
 
 export async function addProductToCart(req, res) {
   const product = res.locals.product; //item virá do middleware de validação do carrinho
-  console.log(product)
+  console.log(product);
 
   try {
     await cartCollection.insertOne(product);
@@ -21,5 +21,15 @@ export async function removeProductFromCart(req, res) {
     res.sendStatus(200);
   } catch (err) {
     console.log(err);
+  }
+}
+
+export async function listCart(req, res) {
+  try {
+    const cart = await cartCollection.find({}).toArray();
+    res.send(cart)
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
   }
 }
--- src/Routes/cartRouter.js
+++ src/Routes/cartRouter.js
@@ -1,9 +1,10 @@
 import {Router} from 'express'
-import { addProductToCart, removeProductFromCart } from '../Controllers/cartController.js'
+import { addProductToCart, removeProductFromCart, listCart } from '../Controllers/cartController.js'
 import { cartValidation } from '../Middlewares/cartValidateMiddleware.js'
 
 const cartRouter = Router()
 
+cartRouter.get("/cart", listCart)
 cartRouter.use(cartValidation)
 cartRouter.post("/addCart", addProductToCart)
 cartRouter.post("/removeCart", removeProductFromCart)
