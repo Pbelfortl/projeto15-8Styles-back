163a83e45115b8be5d7db94320bef96d52817344
Igor de Castro <igordecastro07@gmail.com>
2022-11-26 11:31:32 -0300

    feat: auth valadation

--- src/Routes/cartRouter.js
+++ src/Routes/cartRouter.js
@@ -2,10 +2,12 @@ import {Router} from 'express'
 import { addProductToCart, removeProductFromCart, listCart } from '../Controllers/cartController.js'
 import { addToCartValidation } from '../Middlewares/addToCartValidateMiddleware.js'
 import { removeFromCartValidation } from '../Middlewares/removeFromCartValidateMiddleware.js'
+import authValidate from '../Middlewares/authValidateMiddleware.js'
 
 const cartRouter = Router()
 
 cartRouter.get("/cart", listCart)
+cartRouter.use(authValidate)
 cartRouter.post("/addCart", addToCartValidation, addProductToCart)
 cartRouter.post("/removeCart", removeFromCartValidation, removeProductFromCart)
 
